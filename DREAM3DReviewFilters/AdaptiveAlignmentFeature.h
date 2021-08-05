7d6f492702afbb02b0d77bada323f9ead09acc6d
Michael Jackson <mike.jackson@bluequartz.net>
2021-08-05 14:50:46 -0400

    OCD got the best of me. Had to fix the spelling mistake in the license text
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- DREAM3DReviewFilters/AdaptiveAlignmentFeature.h
+++ DREAM3DReviewFilters/AdaptiveAlignmentFeature.h
@@ -28,7 +28,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * The code contained herein was partially funded by the followig grants:
+ * The code contained herein was partially funded by the following grants:
  *    Czech Science Foundation (GA CR), project no. GBP108/12/G043
  *    Czech Ministry of Education, Youth and Sports (MSMT), project no. LM2015087
  *
