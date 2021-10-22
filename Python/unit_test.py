2dfbcd0adf572ea9f7d3ff090bae3dcf3bfd5e09
Michael Jackson <mike.jackson@bluequartz.net>
2021-10-22 16:52:05 -0400

    Fix Unit test to not crash on macOS. The line causing the crash was not needed
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Python/unit_test.py
+++ Python/unit_test.py
@@ -1,8 +1,7 @@
 # make sure test_filter.py is on PYTHONPATH so it can be found for import
 from Filters import d3d_review_filter_1
-import dream3d.simpl as simpl
 
-pf = simpl.PythonFilter(d3d_review_filter_1.D3DReviewTestFilter())
+import dream3d.simpl as simpl
 
 dca = simpl.DataContainerArray()
 
@@ -10,7 +9,6 @@ dca = simpl.DataContainerArray()
 print(f'Instantiating Filter D3DReviewTestFilter...')
 py_filter = d3d_review_filter_1.D3DReviewTestFilter()
 error_code, message = py_filter.execute(dca)
-
 
 print('Done.')
 
